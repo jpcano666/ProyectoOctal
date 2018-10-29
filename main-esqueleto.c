86647c96d1ac97ff2375785d4c8a92ea0f8b0f22
jpcano1 <jp.cano@uniandes.edu.co>
2018-10-28 22:29:04 -0500

    finished

--- main-esqueleto.c
+++ main-esqueleto.c
@@ -1,8 +1,7 @@
 /*
 PROYECTO 1 FUNDAMENTOS DE INFRAESTRUCTURA TECNOLOGICA - 201820
-Integrante 1 - Codigo integrante 1
-Integrante 2 - Codigo integrante 2
-Integrante 3 - Codigo integrante 3
+Juan Pablo Cano - 201712395 - jp.cano@uniandes.edu.co
+Christer Omar Osorio - 
 */
 
 #define _CRT_SECURE_NO_DEPRECATE
@@ -91,12 +90,25 @@ void convertirAOctal(Datos * datosBin, Datos * datosOct)
 	traductorPos0(0, datosOct->informacion, datosBin->tamanio, datosBin->informacion);
 }
 
+/**
+ * Esta funcion convierte el byte ,cuya posicion en su caracter modulo 3 es 0, si no hay nada despues del primer Byte, traduce los ultimos dos bits junto con un '0'
+ * que se añade para completar el octal. Si hay otro byte despues, guarda estos dos bits sobrantes para leerlos junto con los bits del byte siguiente. La traduccion la hace
+ * tomando los primero tres bits y haciendo un corrimiento de cinco bits a la derecha para tenerlos aislados al final del byte y traducirlos, despues se hace un
+ * corrimiento de 3 bits a la izquierda para eliminar los bits que ya fueron traducidos y nuevamente se hace un corrimiento de 5 bits a la derecha para traducir
+ * los tres bits que sigue, finalmente, se hace un corrimiento de 6 bits a la izquierda para eliminar los primeros dos octales traducidos, y finalmente se corre 5 bits
+ * a la derecha para tener estos dos ultimos bits y un '0' adicional para traduccion.
+ * pre: los datos ingresados no pueden ser nulos ni tampoco el caracter que contiene la traduccion.
+ * @param i Posición del caracter de entrada cuyo modulo con 3 sea cero, por ejemplo, si entra la cadena "abcd" i seria la posición de 'a' y de 'd'.
+ * @param datosOctal caracter que apunta a la primera posicion de los datos traducidos a octal
+ * @param tamanioTotal tamaño total del archivo.
+ * @param datosBin caracter que apunta a la primera posicion de los datos a trducir.
+ **/
 void traductorPos0(int i, unsigned char * datosOctal, int tamanioTotal, unsigned char *datosBin)
 {
   if (i < tamanioTotal)
     {
       unsigned char a = *datosBin;
-      unsigned char b = (a >> 5)+'0';
+      unsigned char b = (a >> 5) + '0';
       unsigned char x = a << 3;
       x = (x >> 5) + '0';
       unsigned char y = a << 6;
@@ -104,20 +116,29 @@ void traductorPos0(int i, unsigned char * datosOctal, int tamanioTotal, unsigned
 			datosOctal[i] = b;
 			datosOctal[i+1] = x;
 
-      printf ("%c ", datosOctal[i]);
-      printf ("%c ", datosOctal[i+1]);
       datosBin++;
       i++;
       if(i==tamanioTotal)
       {
           y = y+'0';
 					datosOctal[i] = y;
-          printf ("%c ", datosOctal[i]);
       }
       traductorPos1 (i, datosOctal , y , tamanioTotal, datosBin);
     }
 }
 
+/**
+ * Esta función convierte el byte a octal, cuya posicion en su caracter modulo 3 es 1, haciendo corrimientos, primero se hace un corrimiento de 7 bits a la derecha para tener el primer bit, despues 
+ * a este bit se le opera con "XOR" con los dos bits que traemos de la función anterior para tener los tres bits juntos. despues corremos un bit a la izquierda para no tener en 
+ * cuenta el bit que ya tradujimos, y lo que queda le hacemos un corrimiento de 5 bits a la derecha para traducir nuestro octal, hacemos los mismo con los siguientes tres bits,
+ * por lo que al final tendremos un bit que correremos 7 bits a la izquierda, luego 5 bits a la derecha, si no hay otro byte despues de este, se traduce este octal, de lo
+ * contrario, se lee en el siguiente byte.
+ * @param i Posición del caracter de entrada cuyo modulo con tres es uno, por ejemplo, si entra la cadena "abcdef" i seria la posición de 'b' y de 'e'.
+ * @param y Son los dos bits que vienen del anterior método.
+ * @param datosOctal caracter que apunta a la primera posicion de los datos traducidos a octal
+ * @param datosBin caracter que apunta a la primera posicion de los datos a trducir.
+ * @param tamanioTotal tamaño total del archivo.
+ **/
 void traductorPos1 ( int i, unsigned char *datosOctal,unsigned char y, int tamanioTotal, unsigned char *datosBin)
 {
   if (i < tamanioTotal)
@@ -134,22 +155,29 @@ void traductorPos1 ( int i, unsigned char *datosOctal,unsigned char y, int taman
 			datosOctal[i]=y;
 			datosOctal[i+1] = c;
 			datosOctal[i+2] = d;
-
-      printf ("%c ", datosOctal[i]);
-      printf ("%c ", datosOctal[i+1]);
-      printf ("%c ", datosOctal[i+2]);
       datosBin++;
       i++;
       if(i==tamanioTotal)
       {
           e = e+'0';
 					datosOctal[i] = e;
-          printf ("%c ", datosOctal[i]);
       }
       traductorPos2(datosOctal,e, datosBin, tamanioTotal, i);
     }
 }
 
+/**
+ * En este método se busca traducri el byte cuya posicion en su caracter modulo 3 es 2, haciendo corrimientos. Primero corremos el byte 6 bits ala derecha y le aplicamos la función
+ * "XOR" con el bit que traemos del metodo anterior para poder traducir nuestro octal. Despues, hacemos un corrimiento de dos bits para deshacernos de los dos bits que ya 
+ * tradujimos y corremos 5 a la derecha, traducimos este octal y pasamos a los siguiente tres bits, corremos 5 bits a la izquierda y luego a la derecha, se traduce este octal.
+ * Finalmente, se devuelve al primer método a verificar si a se terminó la cadena de caracteres, si se acabó, se devuelve la cadena traducida en datosOctal, si no, se repite todo
+ * el procedimiento hasta que se termine todo.
+ * @param i Posición del caracter de entrada cuyo modulo con tres es dos, por ejemplo, si entra la cadena "abcdef" i seria la posición de 'c' y de 'f'.
+ * @param y Es un bit que viene del anterior método.
+ * @param datosOctal caracter que apunta a la primera posicion de los datos traducidos a octal
+ * @param datosBin caracter que apunta a la primera posicion de los datos a trducir.
+ * @param tamanioTotal tamaño total del archivo.
+ **/
 void traductorPos2(unsigned char *datosOctal,unsigned char y, unsigned char *datosBin, int tamanioTotal, int i)
 {
   if (i < tamanioTotal)
@@ -165,9 +193,6 @@ void traductorPos2(unsigned char *datosOctal,unsigned char y, unsigned char *dat
 			datosOctal[i+1] = c;
 			datosOctal[i+2] = d;
 
-      printf ("%c ", datosOctal[i]);
-      printf ("%c ", datosOctal[i+1]);
-      printf ("%c ", datosOctal[i+2]);
       datosBin++;
       i++;
       traductorPos0(i, datosOctal, tamanioTotal, datosBin);
